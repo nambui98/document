32cd54b6c4598d8c16d356c201f68e9a1c6e82ae
nambui98 <bvnam98@gmail.com>
2023-09-30 10:12:29 +0700

    update

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,30 @@
-import {
-    ScrollSpy,
+document.addEventListener('DOMContentLoaded', function () {
+    // Get the fragment identifier from the URL
+    const fragmentId = window.location.hash.substring(1);
 
-    Collapse,
-    initTE,
-} from "tw-elements";
-import {
-    initTE,
-} from "tw-elements";
+    // Scroll to the element with the fragment identifier as its id
+    if (fragmentId) {
+        const element = document.getElementById(fragmentId);
+        if (element) {
+            element.scrollIntoView();
+        }
+    }
+});
 
-initTE({ ScrollSpy, Collapse });
+tailwind.config = {
+    darkMode: "class",
+    theme: {
+        fontFamily: {
+            sans: ["Roboto", "sans-serif"],
+            body: ["Roboto", "sans-serif"],
+            mono: ["ui-monospace", "monospace"],
+        },
+        colors: {
+            primary: '#F2070F',
+            white: '#fff'
+        }
+    },
+    corePlugins: {
+        preflight: false,
+    },
+};
